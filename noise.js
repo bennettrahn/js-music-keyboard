fa9a4e8b6cad301a5be659ac665fc86af7ca08d5
Bennett Rahn <rahnibennett@gmail.com>
2017-11-20 14:02:39 -0800

    done

--- noise.js
+++ noise.js
@@ -1,3 +1,53 @@
+let playNote = function playNote(note) {
+  let audioID = note + 'Audio';
+  let audio = document.getElementById(audioID);
+  if (!audio.paused) {
+    audio.pause();
+    audio.currentTime=0;
+    audio.play();
+  } else {
+    audio.play();
+  }
+}
+
 $(document).ready( function() {
-  // your code here
+  $('.instrument').on('click', 'button', function(event) {
+    playNote($(this).html());
+  });
+
+  $('body').keydown( function(event) {
+    let note = event.key;
+    playNote(note);
+    console.log($('.' + note).html());
+    $('.' + note).addClass('active');
+  });
+
+  $('body').keyup( function(event) {
+    $('.' + event.key).removeClass('active');
+  });
 });
+
+// $('button').click((event) => {
+//   // alert('clicked a thing');
+//   $('#dAudio').get(0).play();
+// });
+
+// $('button').on('click', function() {
+//   // alert('clicked a thing');
+//   $('#cAudio').get(0).play();
+// });
+
+// $(document).ready(() => {
+//   $('#my-list').on('click', 'li', function(event) {
+//     alert(`Got a click on an <li> containing "${$(this).html()}"`);
+//   });
+//
+//   $('#my-list').append($('<li>Herbie Hancock</li>'));
+// });
+
+// $('body').keydown((event) =>  {
+//  if (event.keyCode === 32 || event.keyCode === 13) {
+//    $('#color-box').removeClass().addClass(nextColor());
+//    // alert('You got an space!');
+//  }
+// });
